7a98e3c016dd7a1bdb231f50ce5883283c43292f
MoraJuan <73558008+MoraJuan@users.noreply.github.com>
2022-05-05 12:10:50 -0300

    Add files via upload

--- ViajeroFecuente.py
+++ ViajeroFecuente.py
@@ -13,14 +13,16 @@ class Viajero:
          self.__apellido = apellido
          self.__millas = millas
          
-    def __str__(self):
-        return str(self.__numviajero, self.__millas)
-    
 
+    
+    def getNombre(self):
+        return self.__nombre
     def getNumero(self):
         return self.__numviajero
+    def getMillas(self):
+        return self.__millas
                 
-   
+
     def cantidadTotaldeMillas(self):
         return 'Cantidad de millas realizadas \n' + str(self.__millas)
     
@@ -35,4 +37,26 @@ class Viajero:
         else:
             print('No es posible el cambio las millas a canjear son mayores a las millas totales')
         return self.__millas
+
+    def __gt__(self, other):
+        Valor = False
+        if(self.__millas > other.getMillas()):
+            Valor = True 
+        return Valor
+
+    def __add__(self, other): 
+        return Viajero(self.__numviajero, self.__dni, self.__nombre, self.__apellido, self.__millas + other)
+    
+    def __sub__(self,other):
+        if other <= self.__millas:    
+            return Viajero(self.__numviajero, self.__dni, self.__nombre, self.__apellido, self.__millas - other)
+        else:
+            print('No se puede canjear')
+            return Viajero(self.__numviajero, self.__dni, self.__nombre, self.__apellido, self.__millas)
+    def __eq__(self, other):
+        if self.__millas == other:
+            return True
+        else:
+            return False
+        
         
--- main.py
+++ main.py
@@ -8,9 +8,18 @@ if __name__ == '__main__':
     for fila in reader:
         objeto=Viajero(int(fila[0]),fila[1],fila[2],fila[3],int(fila[4]))
         listaObjeto.append(objeto)
+    Vfmaximo= listaObjeto[0]
+    i=1
+    for i in range(len(listaObjeto)):
+        if listaObjeto[i] > Vfmaximo:
+            Vfmaximo = listaObjeto[i]
+            
+    print('Valor {}'.format(Vfmaximo.getNombre()))        
+            
     numero=int(input('Ingrese numero de VP : '))
     opcion = None
-
+    valor0=0
+    
     for objeto in listaObjeto:
         if numero == objeto.getNumero():
             while opcion!='d':
@@ -26,15 +35,15 @@ if __name__ == '__main__':
                     
                 elif opcion=='b':
                     acum_millas=int(input('Ingrese la cantidad de millas a acumular: '))
-                    cant2=objeto.Acummillas(acum_millas)
-                    print('Nuevas millas acum B:{}'.format(cant2))
+                    listaObjeto[numero-1]= listaObjeto[numero-1] + acum_millas 
+                    print('La cantidad de millas es {}'.format(listaObjeto[numero-1].getMillas()))
                 elif opcion=='c':
                     millas_canjear=int(input('Ingrese millas a canjear: '))
-                    cant3=objeto.canjearMillas(millas_canjear)
-                    print('Millas totales C:{} '.format(cant3))
+                    listaObjeto[numero-1]= listaObjeto[numero-1] - millas_canjear
+                    print('La cantidad de millas es {}'.format(listaObjeto[numero-1].getMillas()))
                 elif opcion=='d':
-                    print('¡Good Bye!')
-                
-        
-
-
+                    valor_ing= int(input('Ingrese valor a comparar'))
+                    if listaObjeto[numero-1] == valor_ing:
+                         print('Son iguales')
+                    else:
+                         print('No son iguales')
